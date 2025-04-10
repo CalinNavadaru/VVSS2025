542ab882fe3a2c63296fe9486c3a9941a7677605
NicolescuAlexandruConstantin <alexnicolescu07@yahoo.com>
2025-04-10 20:53:10 +0300

    Lab3
    solves reqs for lab3

--- code/01_Tasks/src/main/java/tasks/model/TaskList.java
+++ code/01_Tasks/src/main/java/tasks/model/TaskList.java
@@ -5,31 +5,38 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
-public abstract class TaskList implements Iterable<Task>, Serializable  {
+public abstract class TaskList implements Iterable<Task>, Serializable {
     public abstract void add(Task task);
+
     public abstract boolean remove(Task task);
+
     public abstract int size();
+
     public abstract Task getTask(int index);
+
     public abstract List<Task> getAll();
 
     public abstract Iterator<Task> iterator();
 
-    public TaskList incoming(Date from, Date to){
+    public TaskList incoming(Date from, Date to) {
+        if (to.before(from))
+            throw new IllegalArgumentException("End cannot be before start!");
+
         TaskList incomingTasks;
-        if (this instanceof ArrayTaskList){
+        if (this instanceof ArrayTaskList) {
             incomingTasks = new ArrayTaskList();
-        }
-        else {
+        } else {
             incomingTasks = new LinkedTaskList();
         }
 
-        for(int i = 0; i < this.size(); i++){
-            if(getTask(i).nextTimeAfter(from) != null && getTask(i).nextTimeAfter(from).before(to)){
+        for (int i = 0; i < this.size(); i++) {
+            if (getTask(i).nextTimeAfter(from) != null && getTask(i).nextTimeAfter(from).before(to)) {
                 incomingTasks.add(getTask(i));
                 System.out.println(getTask(i).getTitle());
             }
         }
         return incomingTasks;
+
     }
 
 }
---
+++ code/01_Tasks/src/test/java/tasks/model/GetTaskTest.java
@@ -0,0 +1,96 @@
+package tasks.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GetTaskTest {
+
+    private TaskList taskList1;
+    private Task task1, task2, task3, task4;
+
+    @BeforeEach
+    void setUp() {
+        try {
+            task1 = new Task("new task", Task.getDateFormat().parse("2024-10-10 10:10"));
+            task2 = new Task("new task2", Task.getDateFormat().parse("2024-10-10 10:10"));
+            task3 = new Task("new task3", Task.getDateFormat().parse("2024-10-10 10:10"));
+            task4 = new Task("new task4", Task.getDateFormat().parse("2023-10-10 10:10"));
+            task1.setActive(true);
+            task2.setActive(true);
+            task3.setActive(true);
+            task4.setActive(true);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void F02_TC01() {
+        TaskList taskList2 = new LinkedTaskList();
+        try {
+            var x = taskList2.incoming(Task.getDateFormat().parse("2024-10-10 00:00"), Task.getDateFormat().parse("2024-10-11 00:00"));
+            assertEquals(0, x.size());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void F02_TC02() {
+        taskList1 = new ArrayTaskList();
+        try {
+            var x = taskList1.incoming(Task.getDateFormat().parse("2024-10-10 00:00"), Task.getDateFormat().parse("2024-10-11 00:00"));
+            assertEquals(0, x.size());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void F02_TC03() {
+        taskList1 = new ArrayTaskList();
+        taskList1.add(task1);
+        taskList1.add(task2);
+        taskList1.add(task3);
+        taskList1.add(task4);
+        try {
+            taskList1.incoming(Task.getDateFormat().parse("2024-10-10 00:00"), Task.getDateFormat().parse("2024-10-09 00:00"));
+            assertEquals (1,0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void F02_TC04() {
+        taskList1 = new ArrayTaskList();
+        taskList1.add(task1);
+        taskList1.add(task2);
+        taskList1.add(task3);
+        taskList1.add(task4);
+        assertEquals(4,taskList1.size());
+        try {
+            var x = taskList1.incoming(Task.getDateFormat().parse("2024-10-10 00:00"), Task.getDateFormat().parse("2024-10-11 00:00"));
+            assertEquals (3, x.size());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void F02_TC05() {
+        taskList1 = new ArrayTaskList();
+        taskList1.add(task4);
+        try {
+            var x = taskList1.incoming(Task.getDateFormat().parse("2024-10-10 00:00"), Task.getDateFormat().parse("2024-10-11 00:00"));
+            assertEquals (0, x.size());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
